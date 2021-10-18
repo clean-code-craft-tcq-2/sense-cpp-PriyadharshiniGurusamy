79e16acabfe727cbb2600c270c9334150018763a
PriyadharshiniGurusamy <90969124+PriyadharshiniGurusamy@users.noreply.github.com>
2021-10-18 21:18:50 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,17 +1,27 @@
 #include "stats.h"
 #include "algorithm"
+#include "math.h"
 
 Stats Statistics::ComputeStatistics(const std::vector<float>& values) {
     //Implement statistics here
     Stats computedStats;
-    float sum = 0;
-    std::vector<float>::iterator iter = values.begin();
-    computedStats.max = *std::max_element(values.begin() , values.end());
-    computedStats.min = *std::min_element(values.begin() , values.end());
-    for (int i = 0;iter != values.end(); iter++)
+    if(!values.empty())
     {
-        sum = sum + values[i];
+        float sum = 0;
+        std::vector<float>::iterator iter = values.begin();
+        for (int i = 0;iter != values.end(); iter++)
+        {
+            sum = sum + values[i];
+        }
+        computedStats.average = sum/vector.size();
+        computedStats.max = *std::max_element(values.begin() , values.end());
+        computedStats.min = *std::min_element(values.begin() , values.end());
     }
-    computedStats.average = sum/vector.size();
+    else
+    {
+        computedStats.average = NAN;
+        computedStats.max = NAN;
+        computedStats.min = NAN;
+    }
     return computedStats;
 }
