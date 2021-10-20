5e69fcfe32c870d8b187b9396ab87f21c3746da0
PriyadharshiniGurusamy <90969124+PriyadharshiniGurusamy@users.noreply.github.com>
2021-10-20 18:56:29 +0530

    Create Alerts.h

---
+++ Alerts.h
@@ -0,0 +1,37 @@
+#include <vector>
+class IAlerter
+{
+  public:
+  IAlerter();
+  ~IAlerter();
+}
+
+class EmailAlert : public IAlerter
+{
+ public:
+  EmailAlert();
+  ~EmailAlert();
+  bool emailSent;
+}
+
+class LEDAlert : public IAlerter
+{
+ public:
+    LEDAlert();
+    ~LEDAlert();
+    bool ledGlows;
+}
+
+class StatsAlerter 
+{
+  public:
+    /*member functions*/
+    StatsAlerter(float thresholdValue, std::vector<IAlerter*> alerters);
+    ~StatsAlerter();
+    checkAndAlert(std::vector<float>& values);
+    
+  private:
+    /*member variables*/
+    float threshold;
+    std::vector<IAlerter*> alerts;
+}
