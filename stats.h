2f6d8392790f67a7621bc924763462b1d60f6a61
PriyadharshiniGurusamy <90969124+PriyadharshiniGurusamy@users.noreply.github.com>
2021-10-20 19:34:06 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,11 +1,11 @@
 #include <vector>
 
-namespace Statistics {
-    struct Stats
+struct Stats
     {
         float average;
         float max;
         float min;
     };
+namespace Statistics {
     Stats ComputeStatistics(const std::vector<float>& values );
 }
